666f091abd4ef2636242374b2c18d3f951fb130e
kristjan-kure <kristjan.kure.1@gmail.com>
2023-02-01 11:26:20 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// Software asks the user his/her name
+// Software greets the user
+
+Console.WriteLine("Hi, visitor! Please enter your name:"); //output
+string userName = Console.ReadLine(); //input
+
+//Console.WriteLine("Hello" + ", " + userName + "!");
+//string interpolation
+
+Console.WriteLine($"Hello, {userName}!"); //output
+
+
